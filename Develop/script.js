1972ce44933c1002df01dd9739c543730d20b6a8
White Tiger <dayley.zimmermann@gmail.com>
2022-01-05 16:34:02 -0500

    organized for next time

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,12 @@ $('#currentDay').html(moment().format('MMMM Do YYYY'));
 var currentTime = new Date().getHours();
 console.log(currentTime);
 
-var saveBtn = function () {
-  //when i click on save, save to local storage
-  localStorage.setItem("toDoText", JSON.stringify(tasks));
+
+var getStorage = function () {
+  localStorage.getItem("") //retrieve each item from local storage
 };
 
+//function to change the color of time block depending on past/present/future
 var timeChange = function () {
   $('.time-block').each(function() {
     let timeBlock =  parseInt($(this).attr('id').split('-')[1]);
@@ -19,10 +20,21 @@ var timeChange = function () {
       $(this).addClass("present")
     } else if (timeBlock > currentTime) {
       $(this).addClass("future")
-  }});
-  // if (currentTime >= ) {
-  //   $(this).addClass
-  // } else if ()
-};
+    }});
+  };
+  
+  var enterEvent = function () {
+    //when click on middle section, able to enter text.
+  };
 
+  var saveBtn = function () {
+    $(".save").on('click', function() {
+      //do the set item here "when i was doing this homework, i used console and alert alot (edited) because i am trying to get value" -qing
+    });
+    //when i click on save, save to local storage
+    localStorage.setItem("toDoText", JSON.stringify(something));
+    $(this).parent() //something
+  };
+
+getStorage();
 timeChange();
