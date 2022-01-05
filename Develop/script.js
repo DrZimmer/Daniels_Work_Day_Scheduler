1994c6523ea9c49d1b95be89214c3eae443a0378
White Tiger <dayley.zimmermann@gmail.com>
2022-01-05 16:02:51 -0500

    done with timeChange to add classes to the hours for past/present/future

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,28 @@
 //logic for work day scheduler
 //insert current time
-window.setInterval(function () {
-  $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'))
-}, 1000);
+$('#currentDay').html(moment().format('MMMM Do YYYY'));
+
+var currentTime = new Date().getHours();
+console.log(currentTime);
+
+var saveBtn = function () {
+  //when i click on save, save to local storage
+  localStorage.setItem("toDoText", JSON.stringify(tasks));
+};
+
+var timeChange = function () {
+  $('.time-block').each(function() {
+    let timeBlock =  parseInt($(this).attr('id').split('-')[1]);
+    if (timeBlock < currentTime) {
+      $(this).addClass("past")
+    } else if (timeBlock === currentTime) {
+      $(this).addClass("present")
+    } else if (timeBlock > currentTime) {
+      $(this).addClass("future")
+  }});
+  // if (currentTime >= ) {
+  //   $(this).addClass
+  // } else if ()
+};
+
+timeChange();
